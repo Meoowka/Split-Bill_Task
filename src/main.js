c14b08d6b37740a8df73ea204905990e69d81400
meoowka <sasha.medvedev2017@mail.ru>
2024-12-18 15:46:18 +0300

    создал роутинг по страницам, создал для него шаблоны страниц

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import '/src/style.scss'
 import App from './app/App.vue'
-
+import router from "./router/router.js";
 
 createApp(App)
+    .use(router)
     .mount('#app')

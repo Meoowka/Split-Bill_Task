70d4b4a41b9cb366e77a8e9dbf9571acfa7ffbd6
meoowka <sasha.medvedev2017@mail.ru>
2024-12-18 16:07:28 +0300

    добавил разметку главной страницы и стилей для нее

--- src/pages/mainPages/mainPages.vue
+++ src/pages/mainPages/mainPages.vue
@@ -1,5 +1,13 @@
 <template>
-
+  <div class="welcome-container">
+    <h1>Добро пожаловать</h1>
+    <p>Легко делитесь расходами с друзьями и приятно проводите время в кафе.</p>
+    <router-link to="/users">
+      <v-btn class="btn">
+        Создать чек
+      </v-btn>
+    </router-link>
+  </div>
 </template>
 
 <script>
@@ -7,5 +15,5 @@
 </script>
 
 <style scoped lang="scss">
-
+@import "./style.scss";
 </style>

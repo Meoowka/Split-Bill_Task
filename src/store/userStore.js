4c519aed0da6ce5cb9be565555db99a86ff19472
meoowka <sasha.medvedev2017@mail.ru>
2025-01-12 21:04:38 +0300

    Исправил скобочки у мутаций и у актион

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -10,30 +10,26 @@ export default {
     getters: {
         getFirstUserID: state => () => state.user[0]?.id || 0
     },
+    <!--Исправил скобочки-->
     mutations: {
         CLEAR_USERS(state) {
             state.user = [];
         },
-        ADD(state, {name})
-        {
+        ADD(state, {name}) {
             state.user.push(Users(state.user[state.user.length-1]?.id + 1 || 0, name));
         },
-        EDIT(state, {id, name})
-        {
+        EDIT(state, {id, name}) {
             state.user.find(c => c.id === id).name = name || `User ${id + 1}`;
         },
-        REMOVE(state, {id})
-        {
+        REMOVE(state, {id}) {
             state.user = state.user.filter(c => c.id !== id);
         }
     },
     actions: {
-        addUser({ commit }, payload)
-        {
+        addUser({ commit }, payload) {
             commit('ADD', payload);
         },
-        editUser({ commit }, payload)
-        {
+        editUser({ commit }, payload) {
             commit('EDIT', payload);
         },
         removeUser({ commit, getters }, payload) {
